4b1c11e226a8b614617f9ab3343653d6fefddf18
Groove852 <56763360+Groove852@users.noreply.github.com>
2019-10-20 13:22:07 +0300

    Update игра в кости.cpp

--- GroveSt/игра в кости.cpp
+++ GroveSt/игра в кости.cpp
@@ -1,4 +1,3 @@
-﻿
 #include <iostream>
 #include <conio.h>
 #include <time.h>
@@ -18,16 +17,26 @@ int main()
 		getchar();
 		srand((unsigned)time(NULL));
 		int g1 = rand() % 6 + 1;
-		cout << "Результат: " << g1 << endl;
+		//srand((unsigned)time(NULL));
+		int g2 = rand() % 6 + 1;
+		//srand((unsigned)time(NULL));
+		int g3 = rand() % 6 + 1;
+		cout << "Результат: " << g1 << "," << g2 << "," << g3 << endl;
 		cout << "2-й игрок нажмите <Enter> для броска. ";
 		getchar();
 		srand((unsigned)time(NULL));
-		int g2 = rand() % 6 + 1;
-		cout << "Результат: " << g2 << endl;
+		int g4 = rand() % 6 + 1;
+		//srand((unsigned)time(NULL));
+		int g5 = rand() % 6 + 1;
+		//srand((unsigned)time(NULL));
+		int g6 = rand() % 6 + 1;
+		cout << "Результат: " << g4 << "," << g5 << "," << g6 << endl;
+		int gamer1 = g1 + g2 + g3;
+		int gamer2 = g4 + g5 + g6;
 		cout << "Итог: ";
-		if (g1 == g2) cout << "ничья" << endl;
-		if (g1 > g2) cout << "1-й игрок выиграл" << endl;
-		if (g1 < g2) cout << "2-й игрок выиграл" << endl;
+		if (gamer1 == gamer2) cout << "Ничья" << endl;
+		if (gamer1 > gamer2) cout << "Победа первого игрока" << endl;
+		if (gamer1 < gamer2) cout << "Победа второго игрока" << endl;
 		cout << "Хотите сыграть еще раз? Y/N" << endl;
 		cin >> a;
 		
