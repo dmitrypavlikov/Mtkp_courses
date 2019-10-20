7eec267a4c95ea277282c95a86ec2c2079c9fe0c
Groove852 <56763360+Groove852@users.noreply.github.com>
2019-10-20 12:38:43 +0300

    Add files via upload

---
+++ GroveSt/игра в кости.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Поехали!!!";
+	char a;
+	time_t t;
+	do
+	{
+		system("cls");
+		cout << "1-й игрок нажмите <Enter> для броска. ";
+		getchar();
+		srand((unsigned)time(NULL));
+		int g1 = rand() % 6 + 1;
+		cout << "Результат: " << g1 << endl;
+		cout << "2-й игрок нажмите <Enter> для броска. ";
+		getchar();
+		srand((unsigned)time(NULL));
+		int g2 = rand() % 6 + 1;
+		cout << "Результат: " << g2 << endl;
+		cout << "Итог: ";
+		if (g1 == g2) cout << "ничья" << endl;
+		if (g1 > g2) cout << "1-й игрок выиграл" << endl;
+		if (g1 < g2) cout << "2-й игрок выиграл" << endl;
+		cout << "Хотите сыграть еще раз? Y/N" << endl;
+		cin >> a;
+		
+	} while (a == 'Y' || a == 'y');
+	return 0;
+}
